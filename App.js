b1f63d2126b5c56b0a6a5085763fea00ef14bf1e
Aake Moskuvaara <t9moaa00@students.oamk.fi>
2024-09-16 18:31:44 +0300

    valmis

--- App.js
+++ App.js
@@ -1,19 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Button, Appbar } from 'react-native-paper'
+import { getHeaderTitle } from '@react-navigation/elements'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function SecondScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Second Screen</Text>
+    </View>
+  );
+}
+
+function CustomNavigationBar({ navigation, route, options, back }) {
+  const title = getHeaderTitle(options, route.name);
+
+  return (
+    <Appbar.Header>
+      {back ? (
+        <Appbar.BackAction onPress={navigation.goBack} />
+      ) : null}
+      <Appbar.Content title={title} />
+      {route.name === 'Home' ? (
+        <Appbar.Action icon={() => <Icon name="chevron-right" size={30} />} onPress={() => navigation.navigate('SecondScreen')} />
+      ) : null}
+    </Appbar.Header>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            header: (props) => <CustomNavigationBar {...props} />,
+          }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="SecondScreen" component={SecondScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
